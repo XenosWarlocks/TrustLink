767956f9be0b3354dc15080c17ea3005f4ab66ad
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-07-06 05:23:09 +0530

    Add files via upload

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/header-box';
+import TotalBalanceBox from '@/components/total-balance-box';
 import React from 'react';
 
-const Home = () => {
+const Home = async () => {
+    // const loggedIn = await getLoggedInUser();
+    // const account = await getAccount({ appwriteItemId })
+    const loggedIn = { firstName: ' Mannu' };
+    // const account = 
   return (
-    <div>page</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently."
+                />
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1222}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
