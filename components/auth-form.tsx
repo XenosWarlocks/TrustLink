1ecef752976ec7e6db0fd170e3b999120c82c30a
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-07-12 03:23:00 +0530

    Add files via upload

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -21,6 +21,7 @@ import { Button } from "@/components/ui/button"
 import { authFormSchema } from '@/lib/utils';
 import CustomInput from './custom-input';
 import { Loader2 } from 'lucide-react';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 
 const AuthForm = ({ type }: {type: string}) => {
@@ -42,6 +43,28 @@ const AuthForm = ({ type }: {type: string}) => {
         try {
 
             // Sign up with Appwrite & create plaid token
+            if (type === 'sign-up') {
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+                const newUser = await signUp(userData);
+                setUser(newUser);
+            }
+            if(type === 'sign-in') {
+                const response = await signIn({
+                    email: data.email,
+                    password: data.password
+                })
+            }
 
         } catch (error) {
             console.log(error)
